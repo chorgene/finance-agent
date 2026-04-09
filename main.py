cdc044485af0db0c852bfc3854e3528eb5842062
Cheah Chor Gene <chorgenecheah@gmail.com>
2026-04-09 23:59:59 +0800

    WIP: Do not review or delete. Checkpoint continuation for Cheah, Chor Gene

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from finance-agent!")
+from langchain.agents import create_agent
+from langchain_anthropic import ChatAnthropic
+
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatAnthropic(
+    model="claude-haiku-4-5-20251001",
+    max_tokens = "2000"
+)
+
+agent = create_agent(
+    model=model,
+    system_prompt=f"You are a helpful assistant. Your underlying model is {model}. Only mention it when users ask for it. Include the model name and number but not the internal datestamp"
+)
 
 
-if __name__ == "__main__":
-    main()
+result = agent.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": "Hey there, what model are you?"
+        }
+    ]
+})
+
+print(result)
